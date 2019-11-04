77e86043918216395d8b8a16e2027af93b12aa85
Kiarash Hesampour <labeast021@Kiarashs-MacBook-Pro.local>
2019-11-04 08:42:34 -0800

    setting the board and bombs

--- README.md
+++ README.md
@@ -3,3 +3,74 @@
 ## wire frames  :
 
 ![Image of minesweeper](images/IMG_0205.jpeg)
+
+/*----- constants -----*/
+
+
+
+
+
+/*----- app's state (variables) -----*/
+// bomb array /n a simulated board array \n divs and their ids 
+
+
+
+
+
+
+/*----- cached element references -----*/
+
+//  board and it divs 
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+// listen to every click on the board and active the click handler function \n
+
+// listen to reset and active init function 
+
+// 
+
+
+
+
+
+
+
+
+
+/*----- functions -----*/
+// a function to track player click and check for the bombs around. \n if there are none expand and now check for new blocks \n 
+
+
+
+
+
+
+
+
+
+// a function that track player click to mark a block as a bomb \n
+
+
+
+
+
+
+
+// a render function to shows the number of bombs around the clicked block \n shows if a bomb was clicked \n win or loose \n flags \n 
+
+
+
+
+
+
+
+
+
+
+//  initialization function to set the board and time to zero and randomly place bombs in their blocks
--- js/main.js
+++ js/main.js
@@ -1,44 +1,30 @@
-/*----- constants -----*/
+// /////////////// CONSTANT///////////////////////
+// /////////////// VAR //////////////////////////
+let column, row, bombPercentage, board, randomBombs, divVal, divId ;
+var bombs = [];
+// ///////////// CACHED ////////////////////////
 
+let container = document.querySelector(".container");
+// //////////// EVENT LISTENER ////////////////////
 
-
-
-
-/*----- app's state (variables) -----*/
-// bomb array /n a simulated board array \n divs and their ids 
-
-
-
-
-
-
-/*----- cached element references -----*/
-
-//  board and it divs 
-
-
-
-
-/*----- event listeners -----*/
-
-
-
-// listen to every click on the board and active the click handler function \n
-
-// listen to reset and active init function 
-
-// 
-
-
-
-
-
-
-
-
-
-/*----- functions -----*/
-// a function to track player click and check for the bombs around. \n if there are none expand and now check for new blocks \n 
+$(".size").on("click","button",function(){
+  column = parseInt(this.value)
+  column === 40 ? row = 25 : row = column
+})
+$(".difficulty").on("click","button",function(){
+    bombPercentage = this.value*(column*row);
+    board = new Array((column*row) -1).fill(0);
+    for (i=0 ; i <= bombPercentage -1 ; i++){
+        randomBombs = Math.floor(Math.random()*(column*row))
+        bombs.push(randomBombs);
+        board.splice(bombs[i] , 1 , -1);
+    }
+    console.log(bombPercentage);
+})
+$(".start").on("click", function(){
+    render();
+})
+$(".container").on("click", "div", clickHandler)
 
 
 
@@ -46,25 +32,43 @@
 
 
 
+// ////////////// FUNCTIONS /////////////////
+function clickHandler(){
+    if(this.value){
+        divVal = this.value;
+        divId = this.id;
+        console.log(` id = ${this.id} and value = ${this.value}`);
+        if(divVal === 1) console.log("left top corner");
+        else if(divVal === column) console.log("right top corner");
+        else if (divVal === column * row) console.log(" right bottom corner ");
+        else if (divVal === (column*row) - column + 1) console.log(" left bottom corner");
+
+        else if (divVal/column === Math.floor((divVal+1)/column)){
+            console.log("right Wall")
+        }
+        else if((divVal-1)/column === Math.floor(divVal/column)){
+            console.log("left Wall")
+        }
+
+        
+    }
+    else{
+        alert("YOU LOST")
+    }
+}
 
 
-// a function that track player click to mark a block as a bomb \n
 
 
 
-
-
-
-
-// a render function to shows the number of bombs around the clicked block \n shows if a bomb was clicked \n win or loose \n flags \n 
-
-
-
-
-
-
-
-
-
-
-//  initialization function to set the board and time to zero and randomly place bombs in their blocks
+function render(){
+    $(".size, .difficulty").fadeOut(1000);
+    container.style["grid-template-columns"] = `repeat(${column}, 31px)`;
+    container.style["grid-template-row"] = `repeat(${row}, 31px)`;
+    for(i=0 ; i <(column * row) ; i++){
+        squars = document.createElement('div');
+        squars.id = i;
+        container.appendChild(squars);
+        bombs.includes(i) ? squars.classList.add("bomb") : squars.value = i+1;  
+    }}
+    $(".container").fadeOut().fadeIn(4000)
