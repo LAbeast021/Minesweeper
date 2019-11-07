d6ebf5610b22e41fc361a77154514d0ba4aee005
Kiarash Hesampour <labeast021@Kiarashs-MacBook-Pro.local>
2019-11-07 15:40:27 -0800

    reveal complited

--- js/main.js
+++ js/main.js
@@ -9,38 +9,40 @@ const numbers = {
     7 : "pink",
     8 : "olivegreen"
 };
-// /////////////// VAR //////////////////////////
+// /////////////// VAR /////////////////////////////////////////////////////////////////////////////////
 let column, row, bombPercentage, board, randomBombs, divVal, divId,flags,gameClick ;
 var bombs = [];
 flags =[];
 gameClick =true;
-// ///////////// CACHED ////////////////////////
-
+// ///////////// CACHED ////////////////////////////////////////////////////////////////////////////////
 let container = document.querySelector(".container");
-// //////////// EVENT LISTENER ////////////////////
+// //////////// EVENT LISTENER //////////////////////////////////////////////////////////////////////////
 
 $(".size").on("click","button",function(){
   column = parseInt(this.value)
   column === 40 ? row = 25 : row = column
-})
+});
+
 $(".difficulty").on("click","button",function(){
     bombPercentage = this.value*(column*row);
-    board = new Array((column*row) -1).fill(0);
+    board = new Array((column*row)).fill(0);
     for (i=0 ; i <= bombPercentage -1 ; i++){
         randomBombs = Math.floor(Math.random()*(column*row))
         bombs.push(randomBombs);
         board.splice(bombs[i] , 1 , 1);
-    }
-    console.log(bombPercentage);
-})
+    };
+
+$(".start").css("visibility","visible","transition","1s");
+});
+
 $(".start").on("click", function(){
     $(".background").css("display","none")
     this.style.display = "none";
     $("body").css("background-image","url(images/sky.jpg")
     render();
-})
-$(".container").on("click", "div", clickHandler)
+});
 
+$(".container").on("click", "div", clickHandler)
 ////// RIGHT CLICK FOR FLAGS //////////////////////
 
 $('.container ').on("contextmenu",function(evt){
@@ -50,33 +52,14 @@ $('.container ').on("contextmenu",function(evt){
      if (evt.target.textContent === "")$(evt.target).toggleClass("rightclick");
     }
 })
-
-
-
-
-
-// ////////////// FUNCTIONS /////////////////
+// ////////////// FUNCTIONS //////////////////////////////////////////////////////////////////////////////
 function clickHandler(){
 if(this.textContent === "" && gameClick){
     if(this.value && !this.classList.contains("rightclick")){
         divId = parseInt(this.id);
         divVal = this.value;
-        // console.log(` id = ${this.id} and value = ${this.value}`);
-        if(!borderGuard(divVal, divId, this)){
-            if(document.getElementById(divId + column) && document.getElementById(divId - column) ){
-                this.textContent = (board[divId + 1]) + (board[divId - column + 1]) + (board[divId - column ]) + (board[divId - column - 1]) + (board[divId - 1]) + (board[divId + column - 1]) + (board[divId + column]) + (board[divId + column + 1])
-            }
-            else{
-                if(document.getElementById(divId + column)){
-                    this.textContent = (board[divId + 1]) +  (board[divId - 1]) + (board[divId + column - 1]) + (board[divId + column]) + (board[divId + column + 1]);
-                    console.log("divar bala")
-                }
-                else if(document.getElementById(divId - column)){
-                    this.textContent = (board[divId + 1]) +  (board[divId - 1]) + (board[divId - column - 1]) + (board[divId - column]) + (board[divId - column + 1]);
-                    console.log("divar paiin ")
-                }
-            }
-        }    
+        console.log(` id = ${divId} and value = ${divVal} and name ${this.name}`);
+        borderGuard(divId);
     }
     else if(this.classList.contains("rightclick")){ 
         return;
@@ -94,50 +77,76 @@ if(this.textContent === "" && gameClick){
                 gameClick = false;
             }
         }
-    }
-
+    };
     num = parseInt(this.textContent);
-    // if (num || num ===0 )this.style.backgroundColor = "black";
-    // this.classList.add (numbers[num]);
     colorChanger();
-    if(num === 0){
-        rightDown(divId);
-        leftUp(divId);
-    } 
 }
 }
 
 
-function borderGuard (divVal, divId, div){
-    if(divVal === 1){ 
-            div.textContent = board[divId + 1] + board[divId + column] + (board[divId + column + 1])
-       return true;
+function borderGuard (divId){
+    let div = document.getElementById(divId);
+    if(divId === 0 && div.value){
+        div.name = "ltc";
+       div.textContent = board[divId + 1] + board[divId + column] + (board[divId + column + 1])     
     }
-    else if(divVal === column){
-            div.textContent = board[divId - 1] + board[divId + column] + board[divId +( column - 1)]
-        return true;
+    else if(divId+1 === column && div.value){
+        div.name = "rtc"
+        div.textContent = board[divId - 1] + board[divId + column] + board[divId +( column - 1)]
         ;}
-    else if (divVal === column * row) {
-            div.textContent = board[divId - 1]  + board[divId - column] + board[divId - ( column + 1)]
-        return true;
+    else if (divId+1 === column * row && div.value) {
+        div.name = "rbc"
+        div.textContent = board[divId - 1]  + board[divId - column] + board[divId - ( column + 1)]
         ;}
-    else if (divVal === (column*row) - column + 1) {
-            div.textContent = board[divId + 1]  + board[divId - column] + board[divId -  (column - 1)]
-        
-        return true;
+    else if (divId+1 === (column*row) - column + 1 && div.value) {
+        div.name = "lbc"
+        div.textContent = board[divId + 1]  + board[divId - column] + board[divId -  (column - 1)]
         ;}
-    else if (divVal/column === Math.floor((divVal+1)/column)){
-                div.textContent = board[divId - 1] + board[divId + column] + board[divId - column] + board[divId + column - 1] + board[divId - column - 1]
-            return true   
+
+    else if ((divId+1)/column === Math.floor((divId+2)/column) && div.value){
+        div.name = "rw"
+        div.textContent = board[divId - 1] + board[divId + column] + board[divId - column] + board[divId + column - 1] + board[divId - column - 1]  
+    }
+    else if(divId%column === 0 && div.value){
+        div.name = "lw"
+        div.textContent = board[divId + 1] + board[divId + column] + board[divId - column] + board[divId + column + 1] + board[divId - column + 1]   
         }
-    else if((divVal-1)/column === Math.floor(divVal/column)){
-            div.textContent = board[divId + 1] + board[divId + column] + board[divId - column] + board[divId + column + 1] + board[divId - column + 1]
-            return true;
+    else if((divId + column) < (column*row)  && (divId - column) > 0 && div.value){
+        div.textContent = (board[divId + 1]) + (board[divId - column + 1]) + (board[divId - column ]) + (board[divId - column - 1]) + (board[divId - 1]) + (board[divId + column - 1]) + (board[divId + column]) + (board[divId + column + 1])
+    }
+    else if((divId + column) < (column*row) && div.value ){
+        div.textContent = (board[divId + 1]) +  (board[divId - 1]) + (board[divId + column - 1]) + (board[divId + column]) + (board[divId + column + 1]);
+            }
+    else if((divId - column) >= 0 && div.value){
+        div.textContent = (board[divId + 1]) +  (board[divId - 1]) + (board[divId - column - 1]) + (board[divId - column]) + (board[divId - column + 1]);
+            };
+    if(div.textContent == 0 && div.value && div.name ==""){
+                if( (divId+column) < (column*row) && (divId-column) > 0){
+                    borderGuard(divId+column);
+                    div.value=false;
+                    borderGuard(divId-column);
+                    div.value=false;
+                    borderGuard(divId+1);
+                    div.value=false;
+                    borderGuard(divId-1);
+                    div.value=false;
+                }  
+                else if((divId+column) < (column*row)){
+                    borderGuard(divId+1);
+                    div.value=false;
+                    borderGuard(divId-1);
+                    div.value=false;
+
+                }           
+            }
+    else if(div.textContent == 0 && div.value && div.name !==""){
+            return;
         }
-    else{
-           return false;
-        };
-}
+     };
+
+
+
+
 function render(){
     $(".size, .difficulty").css("display","none");
     container.style["grid-template-columns"] = `repeat(${column}, 36px)`;
@@ -148,23 +157,22 @@ function render(){
         squars.classList.add("playground");
         squars.textContent = "";
         container.appendChild(squars);
-         if(!bombs.includes(i)){
-             squars.value = i +1;
-         };
-         if(bombs.includes(i)){
-             document.getElementById(i).style.backgroundColor = "red";
-         }
+        if(!bombs.includes(i)){
+        squars.name = ""; 
+        squars.value = true;}
+        else if(bombs.includes(i)){
+            document.getElementById(i).style.backgroundColor = "red";
+        }
+            
     };   
 };
-
-
-
-function winOrLose(){
-
-}
-
-
-
+function win(){
+    for(i=0 ; i < (column*row) ; i++){
+        if(!bombs.includes(i)){
+            
+        }
+    }
+};
 function colorChanger(){
     for(i=0 ; i< (column*row) ; i++){
         if(document.getElementById(i).textContent !== ""){
@@ -176,92 +184,6 @@ function colorChanger(){
 }
 
 
-
-
- 
-function rightDown(divId){
-    if(document.getElementById(divId).textContent == 0 ){
-if(!document.getElementById(divId+1).classList.contains("rightclick")){
-    document.getElementById(divId+1).textContent = (board[(divId+1) + 1]) + (board[(divId+1) - column + 1]) + (board[(divId+1) - column ]) + (board[(divId+1) - column - 1]) + (board[(divId+1) - 1]) + (board[(divId+1) + column - 1]) + (board[(divId+1) + column]) + (board[(divId+1) + column + 1])
-}
-if(!document.getElementById(divId-1).classList.contains("rightclick")){
-    document.getElementById(divId-1).textContent = (board[(divId-1) + 1]) + (board[(divId-1) - column + 1]) + (board[(divId-1) - column ]) + (board[(divId-1) - column - 1]) + (board[(divId-1) - 1]) + (board[(divId-1) + column - 1]) + (board[(divId-1) + column]) + (board[(divId-1) + column + 1])
-}
-if(!document.getElementById(divId+column).classList.contains("rightclick") && document.getElementById(divId+column)){
-    document.getElementById(divId+column).textContent = (board[(divId+column) + 1]) + (board[(divId+column) - column + 1]) + (board[(divId+column) - column ]) + (board[(divId+column) - column - 1]) + (board[(divId+column) - 1]) + (board[(divId+column) + column - 1]) + (board[(divId+column) + column]) + (board[(divId+column) + column + 1])
-}
-if(!document.getElementById(divId-column).classList.contains("rightclick") && document.getElementById(divId-column) ){
-    document.getElementById(divId-column).textContent = (board[(divId-column) + 1]) + (board[(divId-column) - column + 1]) + (board[(divId-column) - column ]) + (board[(divId-column) - column - 1]) + (board[(divId-column) - 1]) + (board[(divId-column) + column - 1]) + (board[(divId-column) + column]) + (board[(divId-column) + column + 1])
-}
-if(!document.getElementById(divId+(column+1)).classList.contains("rightclick")){
-    document.getElementById(divId+(column+1)).textContent = (board[(divId+(column+1)) + 1]) + (board[(divId+(column+1)) - column + 1]) + (board[(divId+(column+1)) - column ]) + (board[(divId+(column+1)) - column - 1]) + (board[(divId+(column+1)) - 1]) + (board[(divId+(column+1)) + column - 1]) + (board[(divId+(column+1)) + column]) + (board[(divId+(column+1)) + column + 1])
-}
-if(!document.getElementById(divId+(column-1)).classList.contains("rightclick")){
-    document.getElementById(divId+(column-1)).textContent = (board[(divId+(column-1)) + 1]) + (board[(divId+(column-1)) - column + 1]) + (board[(divId+(column-1)) - column ]) + (board[(divId+(column-1)) - column - 1]) + (board[(divId+(column-1)) - 1]) + (board[(divId+(column-1)) + column - 1]) + (board[(divId+(column-1)) + column]) + (board[(divId+(column-1)) + column + 1])
-}
-if(!document.getElementById(divId-(column+1)).classList.contains("rightclick")){
-    document.getElementById(divId-(column+1)).textContent = (board[(divId-(column+1)) + 1]) + (board[(divId-(column+1)) - column + 1]) + (board[(divId-(column+1)) - column ]) + (board[(divId-(column+1)) - column - 1]) + (board[(divId-(column+1)) - 1]) + (board[(divId-(column+1)) + column - 1]) + (board[(divId-(column+1)) + column]) + (board[(divId-(column+1)) + column + 1])
-}
-if(!document.getElementById(divId-(column-1)).classList.contains("rightclick")){
-    document.getElementById(divId-(column-1)).textContent = (board[(divId-(column-1)) + 1]) + (board[(divId-(column-1)) - column + 1]) + (board[(divId-(column-1)) - column ]) + (board[(divId-(column-1)) - column - 1]) + (board[(divId-(column-1)) - 1]) + (board[(divId-(column-1)) + column - 1]) + (board[(divId-(column-1)) + column]) + (board[(divId-(column-1)) + column + 1])
-}
-}
-colorChanger();
-if(document.getElementById(divId+1).textContent == 0){
-    rightDown(divId+1);
-}
-if(document.getElementById(divId+column).textContent ==0 && !isNaN(document.getElementById(divId+column).textContent) ){
-    rightDown(divId+column);
-}
-if(document.getElementById(divId-1).textContent ==0 && !isNaN(document.getElementById(divId+column).textContent) ){
-    leftUp(divId-1);
-}
-if(document.getElementById(divId-column).textContent ==0 && !isNaN(document.getElementById(divId+column).textContent) ){
-    leftUp(divId-column);
-}
-};
-
-function leftUp (divId){
-    if(document.getElementById(divId).textContent == 0 ){
-        if(!document.getElementById(divId+1).classList.contains("rightclick")){
-            document.getElementById(divId+1).textContent = (board[(divId+1) + 1]) + (board[(divId+1) - column + 1]) + (board[(divId+1) - column ]) + (board[(divId+1) - column - 1]) + (board[(divId+1) - 1]) + (board[(divId+1) + column - 1]) + (board[(divId+1) + column]) + (board[(divId+1) + column + 1])
-        }
-        if(!document.getElementById(divId-1).classList.contains("rightclick")){
-            document.getElementById(divId-1).textContent = (board[(divId-1) + 1]) + (board[(divId-1) - column + 1]) + (board[(divId-1) - column ]) + (board[(divId-1) - column - 1]) + (board[(divId-1) - 1]) + (board[(divId-1) + column - 1]) + (board[(divId-1) + column]) + (board[(divId-1) + column + 1])
-        }
-        if(!document.getElementById(divId+column).classList.contains("rightclick")){
-            document.getElementById(divId+column).textContent = (board[(divId+column) + 1]) + (board[(divId+column) - column + 1]) + (board[(divId+column) - column ]) + (board[(divId+column) - column - 1]) + (board[(divId+column) - 1]) + (board[(divId+column) + column - 1]) + (board[(divId+column) + column]) + (board[(divId+column) + column + 1])
-        }
-        if(!document.getElementById(divId-column).classList.contains("rightclick")){
-            document.getElementById(divId-column).textContent = (board[(divId-column) + 1]) + (board[(divId-column) - column + 1]) + (board[(divId-column) - column ]) + (board[(divId-column) - column - 1]) + (board[(divId-column) - 1]) + (board[(divId-column) + column - 1]) + (board[(divId-column) + column]) + (board[(divId-column) + column + 1])
-        }
-        if(!document.getElementById(divId+(column+1)).classList.contains("rightclick")){
-            document.getElementById(divId+(column+1)).textContent = (board[(divId+(column+1)) + 1]) + (board[(divId+(column+1)) - column + 1]) + (board[(divId+(column+1)) - column ]) + (board[(divId+(column+1)) - column - 1]) + (board[(divId+(column+1)) - 1]) + (board[(divId+(column+1)) + column - 1]) + (board[(divId+(column+1)) + column]) + (board[(divId+(column+1)) + column + 1])
-        }
-        if(!document.getElementById(divId+(column-1)).classList.contains("rightclick")){
-            document.getElementById(divId+(column-1)).textContent = (board[(divId+(column-1)) + 1]) + (board[(divId+(column-1)) - column + 1]) + (board[(divId+(column-1)) - column ]) + (board[(divId+(column-1)) - column - 1]) + (board[(divId+(column-1)) - 1]) + (board[(divId+(column-1)) + column - 1]) + (board[(divId+(column-1)) + column]) + (board[(divId+(column-1)) + column + 1])
-        }
-        if(!document.getElementById(divId-(column+1)).classList.contains("rightclick")){
-            document.getElementById(divId-(column+1)).textContent = (board[(divId-(column+1)) + 1]) + (board[(divId-(column+1)) - column + 1]) + (board[(divId-(column+1)) - column ]) + (board[(divId-(column+1)) - column - 1]) + (board[(divId-(column+1)) - 1]) + (board[(divId-(column+1)) + column - 1]) + (board[(divId-(column+1)) + column]) + (board[(divId-(column+1)) + column + 1])
-        }
-        if(!document.getElementById(divId-(column-1)).classList.contains("rightclick")){
-            document.getElementById(divId-(column-1)).textContent = (board[(divId-(column-1)) + 1]) + (board[(divId-(column-1)) - column + 1]) + (board[(divId-(column-1)) - column ]) + (board[(divId-(column-1)) - column - 1]) + (board[(divId-(column-1)) - 1]) + (board[(divId-(column-1)) + column - 1]) + (board[(divId-(column-1)) + column]) + (board[(divId-(column-1)) + column + 1])
-        }
-
-}
-colorChanger();
-
-if(document.getElementById(divId-1).textContent ==0 && !isNaN(document.getElementById(divId+column).textContent) ){
-    leftUp(divId-1);
-}
-// if(document.getElementById(divId-column).textContent ==0 && !isNaN(document.getElementById(divId+column).textContent) ){
-//     leftUp(divId-column);
-// }
-};
-
-
-
-    
 
 
 
